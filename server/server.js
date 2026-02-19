36fadde7b3f94fec2e2f6750762748f63812a0cd
PraveenDommeti <praveendommeti06@gmail.com>
2026-02-19 22:10:29 +0530

    add root route and fix cors

--- server/server.js
+++ server/server.js
@@ -9,8 +9,16 @@ import mysql from 'mysql2/promise';
 dotenv.config();
 
 const app = express();
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+}));
 app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.json({ message: 'Appointment API is running 🚀' });
+});
 
 // --- DATABASE CONNECTION ---
 const pool = mysql.createPool({
